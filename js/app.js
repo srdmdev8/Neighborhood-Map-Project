e075ce9ea37497b24390658c70627c1c0be27400
Shawn R. D. Moran <shawn.r.d.moran@gmail.com>
2018-01-24 10:17:20 -0700

    Add animations to markers and other code to complete my list items.

--- js/app.js
+++ js/app.js
@@ -15,18 +15,51 @@ var myLocations = [
 //Global variables
 var map;
 var markers = [];
+var bounceTimer;
 
 function populateInfoWindow(marker, infowindow) {
-  //Check to make sure the infowindow is not already opened on this marker.
+  // Check to make sure the infowindow is not already opened on this marker.
   if (infowindow.marker != marker) {
+    // Clear the infowindow content to give the streetview time to load.
+    infowindow.setContent('');
     infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div><div>' + marker.address + '</div>');
-    infowindow.open(map, marker);
-    //Make sure the marker property is cleared if the infowindow is closed.
+    // Make sure the marker property is cleared if the infowindow is closed.
     infowindow.addListener('closeclick', function() {
-      infowindow.setMarker = null;
+      infowindow.marker = null;
     });
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    var streetViewService = new google.maps.StreetViewService();
+    var radius = 50;
+    // In case the status is OK, which means the pano was found, compute the
+    // position of the streetview image, then calculate the heading, then get a
+    // panorama from that and set the options
+    function getStreetView(data, status) {
+      if (status == google.maps.StreetViewStatus.OK) {
+        var nearStreetViewLocation = data.location.latLng;
+        var heading = google.maps.geometry.spherical.computeHeading(
+          nearStreetViewLocation, marker.position);
+          infowindow.setContent('<div>' + marker.title + '</div><div>' + marker.address + '</div><div id="pano"></div>');
+          var panoramaOptions = {
+            position: nearStreetViewLocation,
+            pov: {
+              heading: heading,
+              pitch: 30
+            }
+          };
+        var panorama = new google.maps.StreetViewPanorama(
+          document.getElementById('pano'), panoramaOptions);
+      } else {
+        infowindow.setContent('<div>' + marker.title + '</div><div>' + marker.address + '</div>' +
+          '<div>No Street View Found</div>');
+      }
+    }
+    // Use streetview service to get the closest streetview image within
+    // 50 meters of the markers position
+    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+    // Open the infowindow on the correct marker.
+    infowindow.open(map, marker);
   }
+  bounceTimer = setTimeout(function(){ marker.setAnimation(null); }, 1500);
 }
 
 function initMap() {
@@ -39,6 +72,12 @@ function initMap() {
     zoom: 13,
     mapTypeControl: false
   });
+
+  // Style the markers a bit. This will be our listing marker icon.
+  var defaultIcon = makeMarkerIcon('0091ff');
+  // Create a "highlighted location" marker color for when the user
+  // mouses over the marker.
+  var highlightedIcon = makeMarkerIcon('FFFF24');
 
   //The following group uses the location array to create an array of markers on initialize.
   for (var i = 0; i < myLocations.length; i++) {
@@ -54,6 +93,7 @@ function initMap() {
       title: title,
       address: address,
       animation: google.maps.Animation.DROP,
+      icon: defaultIcon,
       id: i
     });
     //Push the marker to our array of markers.
@@ -64,44 +104,64 @@ function initMap() {
     marker.addListener('click', function() {
       populateInfoWindow(this, myInfoWindow);
     });
-
-    var ViewModel = function() {
-      var self = this;
-
-      this.listItems = ko.observableArray();
-      for (var i = 0; i < myLocations.length; i++) {
-        self.listItems.push(myLocations[i].title);
-      }
-
-      self.clickedLocation = function(item) {
-        var marker;
-
-        if (item === myLocations[0].title) {
-          populateInfoWindow(markers[0], myInfoWindow);
-        } else if (item === myLocations[1].title) {
-          populateInfoWindow(markers[1], myInfoWindow);
-        } else if (item === myLocations[2].title) {
-          populateInfoWindow(markers[2], myInfoWindow);
-        } else if (item === myLocations[3].title) {
-          populateInfoWindow(markers[3], myInfoWindow);
-        } else if (item === myLocations[4].title) {
-          populateInfoWindow(markers[4], myInfoWindow);
-        } else if (item === myLocations[5].title) {
-          populateInfoWindow(markers[5], myInfoWindow);
-        } else if (item === myLocations[6].title) {
-          populateInfoWindow(markers[6], myInfoWindow);
-        } else if (item === myLocations[7].title) {
-          populateInfoWindow(markers[7], myInfoWindow);
-        } else if (item === myLocations[8].title) {
-          populateInfoWindow(markers[8], myInfoWindow);
-        } else {
-          populateInfoWindow(markers[9], myInfoWindow);
-        }
-      };
-    };
+    // Two event listeners - one for mouseover, one for mouseout,
+    // to change the colors back and forth.
+    marker.addListener('mouseover', function() {
+      this.setIcon(highlightedIcon);
+    });
+    marker.addListener('mouseout', function() {
+      this.setIcon(defaultIcon);
+    });
   }
+  var ViewModel = function() {
+    var self = this;
+
+    this.listItems = ko.observableArray();
+    for (var i = 0; i < myLocations.length; i++) {
+      self.listItems.push(myLocations[i].title);
+    }
+
+    self.clickedLocation = function(item) {
+
+      if (item === myLocations[0].title) {
+        populateInfoWindow(markers[0], myInfoWindow);
+      } else if (item === myLocations[1].title) {
+        populateInfoWindow(markers[1], myInfoWindow);
+      } else if (item === myLocations[2].title) {
+        populateInfoWindow(markers[2], myInfoWindow);
+      } else if (item === myLocations[3].title) {
+        populateInfoWindow(markers[3], myInfoWindow);
+      } else if (item === myLocations[4].title) {
+        populateInfoWindow(markers[4], myInfoWindow);
+      } else if (item === myLocations[5].title) {
+        populateInfoWindow(markers[5], myInfoWindow);
+      } else if (item === myLocations[6].title) {
+        populateInfoWindow(markers[6], myInfoWindow);
+      } else if (item === myLocations[7].title) {
+        populateInfoWindow(markers[7], myInfoWindow);
+      } else if (item === myLocations[8].title) {
+        populateInfoWindow(markers[8], myInfoWindow);
+      } else {
+        populateInfoWindow(markers[9], myInfoWindow);
+      }
+    };
+  };
   map.fitBounds(bounds);
   ko.applyBindings(new ViewModel());
+}
+
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
+function makeMarkerIcon(markerColor) {
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21,34));
+  return markerImage;
 }
 
 /*const request = require('request');
